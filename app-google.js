72b82674792f3b4d8ce6837b6f9de677233114c1
DannyArroba <danny.gamer11@hotmail.com>
2025-04-29 18:12:47 -0500

    Version 1

--- app-google.js
+++ app-google.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    Tabletop.init({
-      key: '2PACX-1vTPBjarbjJkPXg0xh3sMKzleZAhohscGE5MOPo2CnwQUzArIBVZo8aWccs9yPLKIFKg_MXBNeVh21Tx',
-      callback: function (data, tabletop) {
-        mostrarFicha(data[0]); // Mostrar la primera fila
-      },
-      simpleSheet: true
-    });
+  Tabletop.init({
+    key: '1DXSsg9cRiNvmbC9QBSU4aGEPj4BQrtXU6lFKr1wtYdc',
+    callback: function (data, tabletop) {
+      mostrarFicha(data[0]); // Lee solo la primera fila
+    },
+    simpleSheet: true
   });
-  
-  function mostrarFicha(equipo) {
-    const tabla = document.getElementById('tabla-especificaciones');
-  
-    agregarFila(tabla, 'NOMBRE DEL EQUIPO', equipo['NOMBRE DEL EQUIPO']);
-    agregarFila(tabla, 'CATEGORÍA', equipo['CATEGORIA']);
-    agregarFila(tabla, 'MARCA', equipo['MARCA']);
-    agregarFila(tabla, 'MODELO', equipo['MODELO']);
-    agregarFila(tabla, 'NÚMERO DE SERIE', equipo['NUMERO DE SERIE']);
-    agregarFila(tabla, 'UBICACIÓN ACTUAL', equipo['UBICACIÓN ACTUAL']);
-    agregarFila(tabla, 'FECHA DE ADQUISICIÓN', equipo['FECHA DE ADQUISICIÓN']);
-  }
-  
-  function agregarFila(tabla, campo, valor) {
-    const fila = document.createElement('tr');
-  
-    const th = document.createElement('th');
-    th.textContent = campo;
-  
-    const td = document.createElement('td');
-    td.textContent = valor;
-  
-    fila.appendChild(th);
-    fila.appendChild(td);
-    tabla.appendChild(fila);
-  }
-  
+});
+
+function mostrarFicha(equipo) {
+  const tabla = document.getElementById('tabla-especificaciones');
+
+  agregarFila(tabla, 'NOMBRE DEL EQUIPO', equipo['NOMBRE DEL EQUIPO']);
+  agregarFila(tabla, 'CATEGORIA', equipo['CATEGORIA']);
+  agregarFila(tabla, 'MARCA', equipo['MARCA']);
+  agregarFila(tabla, 'MODELO', equipo['MODELO']);
+  agregarFila(tabla, 'NUMERO DE SERIE', equipo['NUMERO DE SERIE']);
+  agregarFila(tabla, 'UBICACIÓN ACTUAL', equipo['UBICACIÓN ACTUAL']);
+  agregarFila(tabla, 'FECHA DE ADQUISICIÓN', equipo['FECHA DE ADQUISICIÓN']);
+}
+
+function agregarFila(tabla, campo, valor) {
+  const fila = document.createElement('tr');
+
+  const th = document.createElement('th');
+  th.textContent = campo;
+
+  const td = document.createElement('td');
+  td.textContent = valor;
+
+  fila.appendChild(th);
+  fila.appendChild(td);
+  tabla.appendChild(fila);
+}
